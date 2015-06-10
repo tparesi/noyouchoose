c3659eeaffafcfcefb9aeb812ab0c93a3ae5bb24
Lily Riopelle <lilyriopelle@192.168.1.87>
2015-06-10 13:59:05 -0400

    Refactor restaurant filters into subview

--- app/assets/javascripts/views/plans/new.js
+++ app/assets/javascripts/views/plans/new.js
@@ -1,24 +1,10 @@
-NYC.Views.NewPlan = Backbone.View.extend({
+NYC.Views.NewPlan = Backbone.CompositeView.extend({
 
   template: JST["plans/new"],
 
-  events: {
-    "click .filter": "filterRestaurants"
-  },
-
-  filterRestaurants: function(event) {
-    event.preventDefault();
-    filtered = new NYC.Collections.Restaurants();
-    filtered.fetch({
-      data: $(event.currentTarget).parent().serializeJSON(),
-      success: function(response) {
-        debugger
-      }
-    });
-  },
-
   render: function(){
     this.$el.html(this.template());
+    this.addSubview(".choose_restaurants", new NYC.Views.FilterRestaurants());
     return this;
   }
 
