cfbcffb861e4238ba9ca24cb9ab283d9ca7861bf
Lily Riopelle <lilykriopelle@gmail.com>
2015-06-10 16:52:12 -0400

    fix broken facebook oauth

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resource :sessions, only: [:show, :create, :destroy]
     resources :users, only: [:index, :show, :create]
-    # resources :restaurants do
-    #     get "filter", on: :collection
-    # end
     resources :plans, only: [:create, :show]
   end
 
   get "/auth/:provider/callback", to: "sessions#omniauth"
-
   root to: "pages#root"
 end
