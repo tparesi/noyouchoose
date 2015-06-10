3a2660a3c7b582096362a05074aff1721f5b8df3
Lily Riopelle <lilykriopelle@gmail.com>
2015-06-10 15:30:27 -0400

    create plan route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resource :sessions, only: [:show, :create, :destroy]
     resources :users, only: [:index, :show, :create]
-    resources :restaurants do
-        get "filter", on: :collection
-    end
+    # resources :restaurants do
+    #     get "filter", on: :collection
+    # end
+    resources :plans, only: [:create, :show]
   end
 
   get "/auth/:provider/callback", to: "sessions#omniauth"
