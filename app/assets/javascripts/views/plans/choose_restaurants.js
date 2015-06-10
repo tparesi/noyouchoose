606cf6cbd040004ab79dfdf468cd9cb0c3578e2b
Lily Riopelle <lilykriopelle@gmail.com>
2015-06-10 13:59:05 -0400

    Refactor restaurant filters into subview

---
+++ app/assets/javascripts/views/plans/choose_restaurants.js
@@ -0,0 +1,25 @@
+NYC.Views.FilterRestaurants = Backbone.View.extend({
+
+  template: JST["plans/filter_restaurants"],
+
+  events: {
+    "click .filter": "filterRestaurants"
+  },
+
+  filterRestaurants: function(event) {
+    event.preventDefault();
+    filtered = new NYC.Collections.Restaurants();
+    filtered.fetch({
+      data: $(event.currentTarget).parent().serializeJSON(),
+      success: function(response) {
+        debugger
+      }
+    });
+  },
+
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  }
+
+});
--- app/views/api/restaurants/_restaurant.json.jbuilder
+++ app/views/api/restaurants/_restaurant.json.jbuilder
@@ -1,3 +1,4 @@
-json.extract! restaurant, :id, :name, :mobile_url, :image_url, :rating
+json.extract! restaurant, :id, :name, :categories, :mobile_url, :image_url, :rating
 
+json.categories restaurant.categories.map{ |c| c.first }
 json.address restaurant.location.display_address
