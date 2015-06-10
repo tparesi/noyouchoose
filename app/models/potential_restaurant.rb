a3c7232a58cb143680e80a3ddec2eb576220918b
Eric Firth <ericfirth@gmail.com>
2015-06-10 14:25:31 -0400

    Add validations to newer models

--- app/models/potential_restaurant.rb
+++ app/models/potential_restaurant.rb
@@ -1,2 +1,7 @@
 class PotentialRestaurant < ActiveRecord::Base
+  validates :restaurant_id, :plan_id, presence: true
+
+  has_many :swipe_rights, dependent: :destroy
+  belongs_to :restaurant
+  belongs_to :plan
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :users_plans
   has_many :plans, through: :users_plans
   has_many :sessions, dependent: :destroy
+  has_many :swipe_rights, dependent: :destroy
 
   def self.find_or_create_by_auth_hash(auth_hash)
     user = User.find_by(
