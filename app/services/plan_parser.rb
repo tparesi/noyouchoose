c094ab3b285d942b733846730d65f7d640376f6f
Lily Riopelle <lilyriopelle@192.168.1.87>
2015-06-11 09:27:33 -0400

    Fire off an API call for each category filter and disregard duplicate results e.g. japanese and asian fusion crossover

--- app/services/plan_parser.rb
+++ app/services/plan_parser.rb
@@ -18,18 +18,31 @@ class PlanParser
   end
 
   def potential_restaurants
-    query_params = {
-      term: "food",
-      category_filter: @params[:categories].join(","),
-      radius_filter: 3000
-    }
-    Yelp.client.search(@params[:location], query_params).businesses
+    # query_params = {
+    #   term: "food",
+    #   category_filter: @params[:categories].join(","),
+    #   radius_filter: 3000
+    # }
+    # Yelp.client.search(@params[:location], query_params).businesses
+    results = []
+    @params[:categories].each do |category|
+      query_params = {
+        term: "food",
+        category_filter: category,
+        radius_filter: 3000
+      }
+      results.concat(Yelp.client.search(@params[:location], query_params).businesses)
+    end
+
+    results
   end
 
   def add_potential_restaurants
+    already_added = Hash.new(false)
     potential_restaurants.each do |restaurant_data|
       restaurant = Restaurant.find_or_create_by(yelp_id: restaurant_data.id)
-      @plan.potential_restaurants.new(restaurant: restaurant)
+      @plan.potential_restaurants.new(restaurant: restaurant) unless already_added[restaurant]
+      already_added[restaurant] = true
     end
   end
 
