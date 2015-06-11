8ff59a64e780a49f6d5508a3a6b0cbe124f3886c
Lily Riopelle <lilykriopelle@gmail.com>
2015-06-11 09:43:54 -0400

    Add offset to yelp query

--- app/services/plan_parser.rb
+++ app/services/plan_parser.rb
@@ -18,31 +18,19 @@ class PlanParser
   end
 
   def potential_restaurants
-    # query_params = {
-    #   term: "food",
-    #   category_filter: @params[:categories].join(","),
-    #   radius_filter: 3000
-    # }
-    # Yelp.client.search(@params[:location], query_params).businesses
-    results = []
-    @params[:categories].each do |category|
-      query_params = {
-        term: "food",
-        category_filter: category,
-        radius_filter: 3000
-      }
-      results.concat(Yelp.client.search(@params[:location], query_params).businesses)
-    end
-
-    results
+    query_params = {
+      term: "food",
+      category_filter: @params[:categories].join(","),
+      radius_filter: 3000,
+      offset: 1
+    }
+    Yelp.client.search(@params[:location], query_params).businesses
   end
 
   def add_potential_restaurants
-    already_added = Hash.new(false)
     potential_restaurants.each do |restaurant_data|
       restaurant = Restaurant.find_or_create_by(yelp_id: restaurant_data.id)
-      @plan.potential_restaurants.new(restaurant: restaurant) unless already_added[restaurant]
-      already_added[restaurant] = true
+      @plan.potential_restaurants.new(restaurant: restaurant)
     end
   end
 
