df75c1f33901970826cdca9e175a56a6b5be4df9
Eric Firth <ericfirth@gmail.com>
2015-06-20 14:05:40 -0400

    Friendships added

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612181334) do
+ActiveRecord::Schema.define(version: 20150620163505) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string   "title",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "friend_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "friendships", ["friend_id"], name: "index_friendships_on_friend_id"
+  add_index "friendships", ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
+  add_index "friendships", ["user_id"], name: "index_friendships_on_user_id"
 
   create_table "matches", force: :cascade do |t|
     t.integer  "restaurant_id", null: false
