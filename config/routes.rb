21869724181ce2e69eb44a8ecdb1f0510a885943
Lily Riopelle <lilykriopelle@gmail.com>
2015-09-15 14:59:34 -0400

    refactor swiping to swipe model/controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :restaurants, only: [:show]
     resources :swipe_rights, only: [:create, :show]
     resources :swipe_lefts, only: [:create, :show]
+    resources :swipes, only: [:create, :show]
   end
 
   get "/auth/:provider/callback", to: "sessions#omniauth"
