20f0ebdfb86fe32ce1f6b42d96a1bdc631411ba7
Lily Riopelle <lilyriopelle@Lilys-MacBook-Air.local>
2015-09-25 18:44:31 -0400

    add address and categories helpers to restaurant model

--- app/assets/javascripts/models/restaurant.js
+++ app/assets/javascripts/models/restaurant.js
@@ -1,5 +1,13 @@
 NYC.Models.Restaurant = Backbone.Model.extend({
 
-  urlRoot: 'api/restaurants'
+  urlRoot: 'api/restaurants',
+
+  categories: function() {
+    return this.get("categories") || [];
+  },
+
+  address: function() {
+    return this.get("address") || [];
+  }
 
 });
